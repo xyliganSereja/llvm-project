82d13137ca6bc539e0a4536f5b93107d0922c996
XyliganSereja <matva0509@gmail.com>
2025-04-02 21:47:58 +0300

    [Use_arch_btw] 20. Add Use_arch_btwAsmBackend

---
+++ llvm/lib/Target/Use_arch_btw/MCTargetDesc/Use_arch_btwAsmBackend.cpp
@@ -0,0 +1,65 @@
+#include "MCTargetDesc/Use_arch_btwMCTargetDesc.h"
+#include "llvm/ADT/StringSwitch.h"
+#include "llvm/MC/MCAsmBackend.h"
+#include "llvm/MC/MCELFObjectWriter.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCFixupKindInfo.h"
+#include "llvm/MC/MCObjectWriter.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/MC/MCValue.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/EndianStream.h"
+
+using namespace llvm;
+
+namespace {
+class Use_arch_btwAsmBackend : public MCAsmBackend {
+protected:
+  const Target &TheTarget;
+
+public:
+  Use_arch_btwAsmBackend(const Target &T)
+      : MCAsmBackend(llvm::endianness::little), TheTarget(T) {}
+
+  unsigned getNumFixupKinds() const override { return 0; }
+
+  bool writeNopData(raw_ostream &OS, uint64_t Count,
+                    const MCSubtargetInfo *STI) const override {
+    // Cannot emit NOP with size not multiple of 32 bits.
+    if (Count % 4 != 0)
+      return false;
+
+    uint64_t NumNops = Count / 4;
+    for (uint64_t i = 0; i != NumNops; ++i)
+      support::endian::write<uint32_t>(OS, 0x01000000, Endian);
+
+    return true;
+  }
+};
+
+class ELFUse_arch_btwAsmBackend : public Use_arch_btwAsmBackend {
+  Triple::OSType OSType;
+
+public:
+  ELFUse_arch_btwAsmBackend(const Target &T, Triple::OSType OSType)
+      : Use_arch_btwAsmBackend(T), OSType(OSType) {}
+
+  void applyFixup(const MCAssembler &Asm, const MCFixup &Fixup,
+                  const MCValue &Target, MutableArrayRef<char> Data,
+                  uint64_t Value, bool IsResolved,
+                  const MCSubtargetInfo *STI) const override {
+    return;
+  }
+
+  std::unique_ptr<MCObjectTargetWriter>
+  createObjectTargetWriter() const override {}
+};
+
+} // end anonymous namespace
+
+MCAsmBackend *
+llvm::createUse_arch_btwAsmBackend(const Target &T, const MCSubtargetInfo &STI,
+                                   const MCRegisterInfo &MRI,
+                                   const MCTargetOptions &Options) {
+  return new ELFUse_arch_btwAsmBackend(T, STI.getTargetTriple().getOS());
+}
--- llvm/lib/Target/Use_arch_btw/MCTargetDesc/Use_arch_btwMCTargetDesc.h
+++ llvm/lib/Target/Use_arch_btw/MCTargetDesc/Use_arch_btwMCTargetDesc.h
@@ -5,9 +5,19 @@ namespace llvm {
 class MCCodeEmitter;
 class MCContext;
 class MCInstrInfo;
+class MCAsmBackend;
+class MCObjectTargetWriter;
+class MCRegisterInfo;
+class MCSubtargetInfo;
+class MCTargetOptions;
+class Target;
 
 MCCodeEmitter *createUse_arch_btwMCCodeEmitter(const MCInstrInfo &MCII,
                                                MCContext &Ctx);
+MCAsmBackend *createUse_arch_btwAsmBackend(const Target &T,
+                                           const MCSubtargetInfo &STI,
+                                           const MCRegisterInfo &MRI,
+                                           const MCTargetOptions &Options);
 } // namespace llvm
 
 // Defines symbolic names for Use_arch_btw registers.  This defines a mapping
