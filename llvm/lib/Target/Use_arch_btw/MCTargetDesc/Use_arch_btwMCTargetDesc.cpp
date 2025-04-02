e9ad70d6f1fdc887e89037abc202e52201085996
XyliganSereja <matva0509@gmail.com>
2025-04-02 20:02:53 +0300

    [Use_arch_btw] 4. Add empty Subtarget class for Use_arch_btw architecture

--- llvm/lib/Target/Use_arch_btw/MCTargetDesc/Use_arch_btwMCTargetDesc.cpp
+++ llvm/lib/Target/Use_arch_btw/MCTargetDesc/Use_arch_btwMCTargetDesc.cpp
@@ -3,6 +3,7 @@
 #include "Use_arch_btw.h"
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/SubtargerInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 
 using namespace llvm;
@@ -13,11 +14,28 @@ using namespace llvm;
 #define GET_INSTRINFO_ENUM
 #include "Use_arch_btwGenInstrInfo.inc"
 
+#define GET_INSTRINFO_ENUM
+#include "Use_arch_btwGenSubtargetInfo.inc"
+
 static MCRegisterInfo *createUse_arch_btwMCRegisterInfo(const Triple &TT) {
   USE_ARCH_BTW_DUMP_MAGENTA
   MCRegisterInfo *X = new MCRegisterInfo();
   InitUse_arch_btwMCRegisterInfo(X, Use_arch_btw::R0);
   return X;
+}
+
+static MCInstrInfo *createUse_arch_btwMCInstrInfo() {
+  USE_ARCH_BTW_DUMP_MAGENTA
+  MCInstrInfo *X = new MCInstrInfo();
+  InitUse_arch_btwMCInstrInfo(X);
+  return X;
+}
+
+static MCSubtargetInfo *createUse_arch_btwMCSubtargetInfo(const Triple &TT,
+                                                          StringRef CPU,
+                                                          StringRef FS) {
+  USE_ARCH_BTW_DUMP_MAGENTA
+  return createUse_arch_btwMCSubtargetInfoImpl(TT, CPU, /*TuneCPU*/ CPU, FS);
 }
 
 // We need to define this function for linking succeed
@@ -29,4 +47,6 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeUse_arch_btwTargetMC() {
                                     createUse_arch_btwMCRegisterInfo);
   TargetRegistry::RegisterMCInstrInfo(TheUse_arch_btwTarget,
                                       createUse_arch_btwMCInstrInfo);
+  TargetRegistry::RegisterMCSubtargetInfo(TheUse_arch_btwTarget,
+                                          createUse_arch_btwMCSubtargetInfo);
 }
---
+++ llvm/lib/Target/Use_arch_btw/Use_arch_btwSubtarget.cpp
@@ -0,0 +1,17 @@
+#include "Use_arch_btwSubtarget.h"
+#include "Use_arch_btw.h"
+#include "llvm/Target/TargetMachine.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "use_arch_btw-subtarget"
+
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "Use_arch_btwGenSubtargetInfo.inc"
+
+Use_arch_btwSubtarget::Use_arch_btwSubtarget(const StringRef &CPU, const StringRef &TuneCPU,
+                        const StringRef &FS, const TargetMachine &TM)
+    : Use_arch_btwGenSubtargetInfo(TM.getTargetTriple(), CPU, TuneCPU, FS) {
+USE_ARCH_BTW_DUMP_CYAN
+}
