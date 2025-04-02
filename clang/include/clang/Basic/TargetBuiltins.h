438ef598ac4f665e82f6c4cf6938fc96b7432dcf
XyliganSereja <matva0509@gmail.com>
2025-04-02 22:55:48 +0300

    [Use_arch_btw] 25. Add Use_arch_btw ClangBuiltin support (flush and putpixel)

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -402,12 +402,22 @@ namespace clang {
     };
   }
 
+  /// Use_arch_btw builtins
+  namespace Use_arch_btw {
+    enum {
+        LastTIBuiltin = clang::Builtin::FirstTSBuiltin-1,
+#define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+#include "clang/Basic/BuiltinsUse_arch_btw.def"
+        LastTSBuiltin
+    };
+  }
+
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin, Use_arch_btw::LastTSBuilin});
 
 } // end namespace clang.
 
--- clang/lib/Basic/Targets/Use_arch_btw.cpp
+++ clang/lib/Basic/Targets/Use_arch_btw.cpp
@@ -1,9 +1,16 @@
 #include "Use_arch_btw.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+    #define BUILTIN(ID, TYPE, ATTRS)                                               \
+      {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+    #include "clang/Basic/BuiltinsUse_arch_btw.def"
+    };
 
 void Use_arch_btwTargetInfo::getTargetDefines(const LangOptions &Opts,
                                     MacroBuilder &Builder) const {
@@ -11,5 +18,6 @@ void Use_arch_btwTargetInfo::getTargetDefines(const LangOptions &Opts,
 }
 
 ArrayRef<Builtin::Info> Use_arch_btwTargetInfo::getTargetBuiltins() const {
-    return std::nullopt;
+    return llvm::ArrayRef(BuiltinInfo,
+        clang::Use_arch_btw::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
