6ef0ac623afdf9d565221ce08bbbd2d3aefa9f29
XyliganSereja <matva0509@gmail.com>
2025-04-02 21:09:28 +0300

    [Use_arch_btw] 13. Add Calling Convention, MachineFunction and Lowering Functions

---
+++ llvm/lib/Target/Use_arch_btw/Use_arch_btwMachineFunctionInfo.cpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "llvm/CodeGen/MachineFunction.h"
+#include <vector>
+
+namespace llvm {
+
+/// Use_arch_btwFunctionInfo - This class is derived from MachineFunction
+/// private Use_arch_btw target-specific information for each MachineFunction.
+class Use_arch_btwFunctionInfo : public MachineFunctionInfo {
+  virtual void anchor();
+
+  bool ReturnStackOffsetSet = false;
+  unsigned ReturnStackOffset = -1U;
+
+  /// FrameIndex for start of varargs area
+  int VarArgsFrameIndex = 0;
+  /// Size of the save area used for varargs
+  int VarArgsSaveSize = 0;
+  /// Size of stack frame to save callee saved registers
+  unsigned CalleeSavedStackSize = 0;
+
+public:
+  Use_arch_btwFunctionInfo() {}
+  explicit Use_arch_btwFunctionInfo(MachineFunction &MF) {}
+  ~Use_arch_btwFunctionInfo() {}
+
+  void setVarArgsFrameIndex(int Off) { VarArgsFrameIndex = Off; }
+  int getVarArgsFrameIndex() const { return VarArgsFrameIndex; }
+
+  void setVarArgsSaveSize(int Size) { VarArgsSaveSize = Size; }
+  int getVarArgsSaveSize() const { return VarArgsSaveSize; }
+
+  unsigned getCalleeSavedStackSize() const { return CalleeSavedStackSize; }
+  void setCalleeSavedStackSize(unsigned Size) { CalleeSavedStackSize = Size; }
+
+  void setReturnStackOffset(unsigned Off) {
+    assert(!ReturnStackOffsetSet && "Return stack offset set twice");
+    ReturnStackOffset = Off;
+    ReturnStackOffsetSet = true;
+  }
+
+  unsigned getReturnStackOffset() const {
+    assert(ReturnStackOffsetSet && "Return stack offset not set");
+    return ReturnStackOffset;
+  }
+
+  // unsigned MaxCallStackReq = 0;
+};
+
+} // end namespace llvm
